37c54de9e972bf5426e86ee06eb6cb3b7cbed9ec
Ori <orysiastakh@gmail.com>
2018-07-25 17:41:32 +0300

    need some fixes

--- index.js
+++ index.js
@@ -108,3 +108,146 @@ getGithubCredentials(function(){
         });
       });
     }
+    function createRepo(callback) {
+        var argv = require('minimist')(process.argv.slice(2));
+      
+        var questions = [
+          {
+            type: 'input',
+            name: 'name',
+            message: 'Enter a name for the repository:',
+            default: argv._[0] || files.getCurrentDirectoryBase(),
+            validate: function( value ) {
+              if (value.length) {
+                return true;
+              } else {
+                return 'Please enter a name for the repository';
+              }
+            }
+          },
+          {
+            type: 'input',
+            name: 'description',
+            default: argv._[1] || null,
+            message: 'Optionally enter a description of the repository:'
+          },
+          {
+            type: 'list',
+            name: 'visibility',
+            message: 'Public or private:',
+            choices: [ 'public', 'private' ],
+            default: 'public'
+          }
+        ];
+      
+        inquirer.prompt(questions).then(function(answers) {
+          var status = new Spinner('Creating repository...');
+          status.start();
+      
+          var data = {
+            name : answers.name,
+            description : answers.description,
+            private : (answers.visibility === 'private')
+          };
+      
+          github.repos.create(
+            data,
+            function(err, res) {
+              status.stop();
+              if (err) {
+                return callback(err);
+              }
+              return callback(null, res.ssh_url);
+            }
+          );
+        });
+      }
+
+      function createGitignore(callback) {
+        var filelist = _.without(fs.readdirSync('.'), '.git', '.gitignore');
+      
+        if (filelist.length) {
+          inquirer.prompt(
+            [
+              {
+                type: 'checkbox',
+                name: 'ignore',
+                message: 'Select the files and/or folders you wish to ignore:',
+                choices: filelist,
+                default: ['node_modules', 'bower_components']
+              }
+            ]
+          ).then(function( answers ) {
+              if (answers.ignore.length) {
+                fs.writeFileSync( '.gitignore', answers.ignore.join( '\n' ) );
+              } else {
+                touch( '.gitignore' );
+              }
+              return callback();
+            }
+          );
+        } else {
+          touch('.gitignore');
+          return callback();
+        }
+      }
+
+      function setupRepo( url, callback ) {
+        var status = new Spinner('Setting up the repository...');
+        status.start();
+      
+        git
+          .init()
+          .add('.gitignore')
+          .add('./*')
+          .commit('Initial commit')
+          .addRemote('origin', url)
+          .push('origin', 'master')
+          .then(function(){
+            status.stop();
+            return callback();
+          });
+      }
+
+      function githubAuth(callback) {
+        getGithubToken(function(err, token) {
+          if (err) {
+            return callback(err);
+          }
+          github.authenticate({
+            type : 'oauth',
+            token : token
+          });
+          return callback(null, token);
+        });
+      }
+      
+      githubAuth(function(err, authed) {
+        if (err) {
+          switch (err.code) {
+            case 401:
+              console.log(chalk.red('Couldn\'t log you in. Please try again.'));
+              break;
+            case 422:
+              console.log(chalk.red('You already have an access token.'));
+              break;
+          }
+        }
+        if (authed) {
+          console.log(chalk.green('Sucessfully authenticated!'));
+          createRepo(function(err, url){
+            if (err) {
+              console.log('An error has occured');
+            }
+            if (url) {
+              createGitignore(function() {
+                setupRepo(url, function(err) {
+                  if (!err) {
+                    console.log(chalk.green('All done!'));
+                  }
+                });
+              });
+            }
+          });
+        }
+      });
